73661b538b4cdceb1e8e3b46ea0ed0302dadf642
Avishek Saha <barshan23@gmail.com>
2017-08-20 21:15:09 +0530

    getting the basics up and runnig

---
+++ snake.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Snake")
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+#		print event
+
+pygame.quit()
