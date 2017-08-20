5cab176cd6ffa515b3fea8744d3ca54047c1b925
Avishek Saha <barshan23@gmail.com>
2017-08-20 22:43:33 +0530

    tweaking move

--- snake.py
+++ snake.py
@@ -16,6 +16,7 @@ gameExit = False
 
 lead_x, lead_y = [300,300]
 lead_x_change = 0
+lead_y_change = 0
 
 clock = pygame.time.Clock()
 
@@ -27,14 +28,19 @@ while not gameExit:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
 				lead_x_change = -10
+				lead_y_change = 0
 			if event.key == pygame.K_RIGHT:
 				lead_x_change = 10
+				lead_y_change = 0
 			if event.key == pygame.K_UP:
-				lead_y -= 10
+				lead_y_change = -10
+				lead_x_change = 0
 			if event.key == pygame.K_DOWN:
-				lead_y += 10
+				lead_y_change = 10
+				lead_x_change = 0
 
 	lead_x += lead_x_change
+	lead_y += lead_y_change
 	gameDisplay.fill(WHITE)
 	pygame.draw.rect(gameDisplay, BLACK, [lead_x,lead_y,10,10])
 	pygame.display.update()
